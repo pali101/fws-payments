c4fe8453d6c95dde2f46ebf4e05208c1c13012c2
Aashish Paliwal <aashish.paliwal@live.com>
2025-07-26 08:20:26 +0530

    refactor: move IValidator interface to src/interfaces and generate IPayments

--- src/Payments.sol
+++ src/Payments.sol
@@ -11,29 +11,7 @@ import "./RateChangeQueue.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Permit.sol";
 import "./Errors.sol";
-
-interface IValidator {
-    struct ValidationResult {
-        // The actual payment amount determined by the validator after validation of a rail during settlement
-        uint256 modifiedAmount;
-        // The epoch up to and including which settlement should occur.
-        uint256 settleUpto;
-        // A placeholder note for any additional information the validator wants to send to the caller of `settleRail`
-        string note;
-    }
-
-    function validatePayment(
-        uint256 railId,
-        uint256 proposedAmount,
-        // the epoch up to and including which the rail has already been settled
-        uint256 fromEpoch,
-        // the epoch up to and including which validation is requested; payment will be validated for (toEpoch - fromEpoch) epochs
-        uint256 toEpoch,
-        uint256 rate
-    ) external returns (ValidationResult memory result);
-
-    function railTerminated(uint256 railId, address terminator, uint256 endEpoch) external;
-}
+import "./interfaces/IValidator.sol";
 
 // @title Payments contract.
 contract Payments is Initializable, UUPSUpgradeable, OwnableUpgradeable, ReentrancyGuardUpgradeable {
---
+++ src/interfaces/IValidator.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+pragma solidity ^0.8.27;
+
+interface IValidator {
+    struct ValidationResult {
+        // The actual payment amount determined by the validator after validation of a rail during settlement
+        uint256 modifiedAmount;
+        // The epoch up to and including which settlement should occur.
+        uint256 settleUpto;
+        // A placeholder note for any additional information the validator wants to send to the caller of `settleRail`
+        string note;
+    }
+
+    function validatePayment(
+        uint256 railId,
+        uint256 proposedAmount,
+        // the epoch up to and including which the rail has already been settled
+        uint256 fromEpoch,
+        // the epoch up to and including which validation is requested; payment will be validated for (toEpoch - fromEpoch) epochs
+        uint256 toEpoch,
+        uint256 rate
+    ) external returns (ValidationResult memory result);
+
+    function railTerminated(uint256 railId, address terminator, uint256 endEpoch) external;
+}
