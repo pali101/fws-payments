6f06aa95674b67ac3d98055a6bc2bf94e27ebfef
Aashish Paliwal <aashish.paliwal@live.com>
2025-08-16 19:21:27 +0530

    feat: add ERC-3009 support to Mock ERC-20

--- test/mocks/MockERC20.sol
+++ test/mocks/MockERC20.sol
@@ -2,12 +2,74 @@
 pragma solidity ^0.8.27;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 
+/**
+ * @title MockERC20
+ * @dev A mock ERC20 token with permit (ERC-2612) and transferWithAuthorization (ERC-3009) functionality for testing purposes.
+ */
 contract MockERC20 is ERC20Permit {
+    // --- ERC-3009 State and Constants ---
+    mapping(address => mapping(bytes32 => bool)) private _authorizationStates;
+
+    bytes32 private constant _TRANSFER_WITH_AUTHORIZATION_TYPEHASH = keccak256(
+        "TransferWithAuthorization(address from,address to,uint256 value,uint256 validAfter,uint256 validBefore,bytes32 nonce)"
+    );
+
+    // --- ERC-3009 Event ---
+    event AuthorizationUsed(address indexed authorizer, bytes32 indexed nonce);
+
     constructor(string memory name, string memory symbol) ERC20(name, symbol) ERC20Permit(name) {}
 
     // Mint tokens for testing
     function mint(address to, uint256 amount) public {
         _mint(to, amount);
+    }
+
+    // --- ERC-3009 Implementation ---
+
+    /**
+     * @notice Execute a transfer with a signed authorization
+     * @param from          Payer's address (Authorizer)
+     * @param to            Payee's address
+     * @param value         Amount to be transferred
+     * @param validAfter    The time after which this is valid (unix time)
+     * @param validBefore   The time before which this is valid (unix time)
+     * @param nonce         Unique nonce
+     * @param v             v of the signature
+     * @param r             r of the signature
+     * @param s             s of the signature
+     */
+    function transferWithAuthorization(
+        address from,
+        address to,
+        uint256 value,
+        uint256 validAfter,
+        uint256 validBefore,
+        bytes32 nonce,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external {
+        require(block.timestamp > validAfter, "EIP3009: authorization not yet valid");
+        require(block.timestamp < validBefore, "EIP3009: authorization expired");
+        require(!_authorizationStates[from][nonce], "EIP3009: authorization already used");
+
+        bytes32 structHash = keccak256(
+            abi.encode(_TRANSFER_WITH_AUTHORIZATION_TYPEHASH, from, to, value, validAfter, validBefore, nonce)
+        );
+
+        bytes32 digest = EIP712._hashTypedDataV4(structHash);
+        address signer = ECDSA.recover(digest, v, r, s);
+        require(signer == from, "Invalid signature");
+
+        _authorizationStates[from][nonce] = true;
+        emit AuthorizationUsed(from, nonce);
+
+        _transfer(from, to, value);
+    }
+
+    function authorizationState(address authorizer, bytes32 nonce) external view returns (bool) {
+        return _authorizationStates[authorizer][nonce];
     }
 }
